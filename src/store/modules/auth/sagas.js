820b0216e9df5aa8c0964961cee2fe8879c47750
João Phellipe de Freitas Pinto <phellipe@ufersa.edu.br>
2019-09-13 14:13:15 -0300

    half of auth

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,9 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import api from '~/services/api';
 
-export default all([]);
+export function* singIN({ payload }) {
+  const { email, password } = payload;
+  const response = yield call(api.post, 'sessions');
+}
+
+export default all([takeLatest('@auth/SING_IN_REQUEST', singIN)]);
