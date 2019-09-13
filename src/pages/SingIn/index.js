c88db1c97b67b8af2822e2d0a8c07d4f7a5ee4cd
João Phellipe de Freitas Pinto <phellipe@ufersa.edu.br>
2019-09-13 10:40:11 -0300

    added unform, yup

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('É obrigatório informar um e-mail'),
+  password: Yup.string()
+    .min(6, 'A senha deve conter no mínimo 6 caracteres')
+    .required('É obrigatótio informar a senha'),
+});
 
 export default function SingIn() {
-  return <h1>Login</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('É obrigatório informar um nome'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('É obrigatório informar um e-mail'),
+  password: Yup.string()
+    .min(6, 'A senha deve conter no mínimo 6 caracteres')
+    .required('É obrigatótio informar a senha'),
+});
 
 export default function SingUp() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
