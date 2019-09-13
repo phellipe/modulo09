04e9342fb6054f5cd8d3b5641c6e82fe07a6b256
João Phellipe de Freitas Pinto <phellipe@ufersa.edu.br>
2019-09-13 11:10:52 -0300

    added flux architeture with sagas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
 import history from './services/history';
+
+import store from './store';
 import Routes from './routes';
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyles />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </Provider>
   );
 }
 
